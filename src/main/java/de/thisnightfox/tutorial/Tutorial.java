bd448f8a72d06a10a1ed555915a8b4a77910e276
CraftCityHD <owner@citycrafting.de>
2023-01-07 21:51:21 +0100

    Added onDisable Message

--- src/main/java/de/thisnightfox/tutorial/Tutorial.java
+++ src/main/java/de/thisnightfox/tutorial/Tutorial.java
@@ -14,5 +14,6 @@ public final class Tutorial extends JavaPlugin {
     @Override
     public void onDisable() {
         getLogger().info("Plugin deaktiviert");
+        System.out.println("Say goodby");
     }
 }
